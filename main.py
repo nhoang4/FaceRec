675e4baed3e261a2886eed88f1a506eef84a20c5
nhoang4 <huunhan1904@gmail.com>
2023-05-31 10:34:43 +0700

    Library altercation from facenet to deepface

--- main.py
+++ main.py
@@ -1,64 +1,18 @@
+from deepface import DeepFace
 import cv2
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.imagenet_utils import preprocess_input
-from sklearn.preprocessing import Normalizer
 from pathlib import Path
 
+image1 = cv2.imread(str(Path.cwd()) + '/Testimages/image1.jpg')
 
-# Load FaceNet model
-model = load_model(str(Path.cwd())+'/facenet_keras.h5')
+image2 = cv2.imread(str(Path.cwd()) + '/Testimages/image2.jpg')
 
-# Normalize face
-normalizer = Normalizer(norm='l2')
+image3 = cv2.imread(str(Path.cwd()) + '/Testimages/image3.jpg')
 
-def extract_face_features(img):
-    # Convert the image to RGB
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # Detect faces in the image
-    face_cascade = cv2.CascadeClassifier(str(Path.cwd()) + '/haarcascade_frontalface_default.xml')
-    #to be determined of the scales
-    faces = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    if len(faces) > 0:
-        (x, y, w, h) = faces[0]
-        # Extract the face region from the image
-        face = img[y:y+h, x:x+w]
-        # Resize the face image to the input shape required by FaceNet model
-        face = cv2.resize(face, (160, 160))
-        face = face.astype('float32')
-        # Preprocess the face image
-        face = preprocess_input(face)
-        return face
-    else:
-        return None
+image4 = cv2.imread(str(Path.cwd()) + '/Testimages/image4.jpg')
 
+image5 = cv2.imread(str(Path.cwd()) + '/Testimages/image5.jpg')
 
+image6 = cv2.imread(str(Path.cwd()) + '/Testimages/image6.jpg')
 
-def get_face_embeddings(face):
-    face = np.expand_dims(face, axis=0)
-    embeddings = model.predict(face)
-    embeddings = normalizer.transform(embeddings)
-    return embeddings
-
-def compare_faces(embeddings1, embeddings2):
-    distance = np.linalg.norm(embeddings1 - embeddings2)
-    similarity = 1 / (1 + distance)
-    return similarity
-
-
-
-
-
-image1 = cv2.imread('image1.jpg')
-image2 = cv2.imread('image2.jpg')
-face1 = extract_face_features(image1)
-face2 = extract_face_features(image2)
-if face1 is not None and face2 is not None:
-    fe1 = get_face_embeddings(face1)
-    fe2 = get_face_embeddings(face2)
-    num = compare_faces(fe, fe)
-    print('Similarity:', num)
-else:
-    print('No faces detected in one or both of the images.')
+result = DeepFace.verify(image3, image6)
+print(result)
