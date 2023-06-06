8ae442eb3f43f94500cfd43561252349856c07f0
nhoang4 <huunhan1904@gmail.com>
2023-06-06 09:43:00 +0700

    Added code to deal with unoriented images

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
-import cv2
 import dlib
 from pathlib import Path
 import numpy as np
 from mtcnn import MTCNN
-from retinaface import RetinaFace
+from PIL import Image
 
 
 def detect_face(image):
     detector = MTCNN()
-    results = detector.detect_faces(image)
+    image_rgb = image[:, :, ::-1]  # Convert BGR to RGB
+    results = detector.detect_faces(image_rgb)
     if len(results) == 0:
-        return None
+        try_rotated_image = Image.fromarray(np.rot90(image))
+        faces = detector.detect_faces(try_rotated_image)
+        if len(faces) == 0:
+            return None
+        else:
+            x, y, w, h = faces[0]['box']
+            return (x, y, w, h)
     else:
         x, y, w, h = results[0]['box']
         return dlib.rectangle(x, y, x + w, y + h)
-
-
-def detect_face2(image):
-    detector = RetinaFace.detect_faces
-    results = detector(image, model='resnet50')
-
-    if len(results) == 0:
-        return None
-    else:
-        # Extract the bounding box of the first face detected
-        box = results[0]['box']
-        x, y, w, h = box[0], box[1], box[2], box[3]
-        return (x, y, w, h)
 
 
 def compare_faces(image1, image2):
@@ -46,25 +39,18 @@ def compare_faces(image1, image2):
     shape1 = shape_predictor(image1, face1)
     face_descriptor1 = face_recognizer.compute_face_descriptor(image1, shape1)
 
-
     shape2 = shape_predictor(image2, face2)
     face_descriptor2 = face_recognizer.compute_face_descriptor(image2, shape2)
-
 
     face_descriptor1 = np.array(face_descriptor1)
     face_descriptor2 = np.array(face_descriptor2)
 
-
     distance = np.linalg.norm(face_descriptor1 - face_descriptor2)
-
-
     similarity_percentage = (1 - distance) * 100
+
     print("Similarity: {:.2f}%".format(similarity_percentage))
 
 
-
-
-
-image1 = dlib.load_rgb_image(str(Path.cwd()) + '/Test_Images/ID1.jpg')
-image2 = dlib.load_rgb_image(str(Path.cwd()) + '/Test_Images/ID22.jpg')
+image1 = dlib.load_rgb_image(str(Path.cwd()) + '/Test_Images/image3.jpg')
+image2 = dlib.load_rgb_image(str(Path.cwd()) + '/Test_Images/image4.jpg')
 compare_faces(image1, image2)
